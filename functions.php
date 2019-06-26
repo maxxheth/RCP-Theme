d38f0012e17fc7bacbad9ff6365ab110906c25ce
Maximillian Heth <maximillian.heth@gmail.com>
2019-06-25 21:49:44 -0500

    Registered menu

--- functions.php
+++ functions.php
@@ -3,6 +3,21 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Set up menu area.
+
+function register_rcp_menu() {
+
+    register_nav_menus(array(
+
+        'primary-menu' => __( 'Primary Menu' ),
+        'secondary-menu' => __( 'Secondary Menu')
+
+    ));
+
+}
+
+add_action('init', 'register_rcp_menu');
+
 /*
  * Switch default core markup for search form, comment form, and comments
  * to output valid HTML5.
